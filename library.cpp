f7bf42b9fdf6ea5f8fada4b168dc0d35d3188ce4
schneeball231 <schneeball231@outlook.com>
2025-10-14 23:55:04 +0200

    Initial commit

---
+++ library.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <Windows.h>
+
+bool __stdcall DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved) {
+    static FILE *p_file{nullptr};
+
+    if (reason == DLL_PROCESS_ATTACH) {
+        AllocConsole();
+        freopen_s(&p_file, "CONOUT$", "w", stdout);
+
+        while (!GetAsyncKeyState(VK_END)) {
+        }
+
+        FreeLibrary(instance);
+    } else if (reason == DLL_PROCESS_DETACH) {
+        fclose(p_file);
+        FreeConsole();
+    }
+
+    return true;
+}
